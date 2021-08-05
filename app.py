8c5cd2166d0f01df5a2175466f17c206821b31cd
shubham sangulage <43124045+S-Instro@users.noreply.github.com>
2021-08-05 16:34:36 +0530

    about page
    click on explore and will redirect to about page

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+    
+@app.route("/About")
+def About():
+    return render_template('about.html')
 
 if __name__=="__main__":
     app.run(debug=True,port=5312)
